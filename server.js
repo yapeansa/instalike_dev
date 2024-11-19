a2253e948c4d885eb87a615b4c0cf373790a83fb
yapeansa <yago.pereira@estudante.ufjf.br>
2024-11-18 21:17:33 -0300

    Alterando mensagem.

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.listen(3000, () => {
 });
 
 app.get("/api", (req, res) => {
-    res.status(200).send("<h1>Criando uma rota inicial.</h1>");
+    res.status(200).send("Criando uma rota inicial.");
 });
